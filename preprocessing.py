3b84149aa3d6a75dbacf608e9dafc82ac4f84068
python_dev <pythondev098@gmail.com>
2024-05-09 17:46:54 +0330

    edtied preprocessing and main

--- preprocessing.py
+++ preprocessing.py
@@ -31,49 +31,9 @@ class Preprocessing:
                                    'external-detection-corpus\\suspicious-document'
         self.source_parts_dir_name = os.listdir(source_dir_name_path)
         # suspicious_parts_dir_name = os.listdir(suspicious_dir_name_path)
-        self.preprocessing1(source_dir_name_path)
-        # self.preprocessing(source_dir_name_path)
-        # self.preprocessing(suspicious_dir_name_path)
+        self.preprocessing(source_dir_name_path)
 
     def preprocessing(self, path):
-        for part in tqdm(self.source_parts_dir_name):
-            print("part : ", part)
-            dir_part_path = f'{path}\\{part}'
-            os.chdir(dir_part_path)
-            source_files = [f for f in os.listdir(dir_part_path) if f.endswith('.txt')]
-            for file in tqdm(source_files):
-                file_name = f'{path}\\{part}\\{file}'
-                # print(sss)
-                with open(f'{file_name}', 'r', encoding="utf8") as txt_file:
-                    # print(txt_file.read())
-                    """
-                    s = " ".join(line.rstrip().lower() for line in tqdm(txt_file))
-                    # s = s.lower()  # downcase
-                    tokens = nltk.tokenize.word_tokenize(s)  # split string into words (tokens)
-                    tokens = [t for t in tokens if len(t) > 2]  # remove short words, they're probably not useful
-                    tokens = [wordnet_lemmatizer.lemmatize(t) for t in tokens]  # put words into base form
-                    tokens = [t for t in tokens if t not in stopwords]  # remove stopwords
-                    print("tokens: ", tokens)
-                    """
-                    text = " ".join(line.rstrip().lower() for line in tqdm(txt_file))
-                    # print(text)
-                    # Remove numbers
-                    text = ''.join([i for i in text if not i.isdigit()])
-                    # Remove punctuation
-                    text = text.translate(str.maketrans('', '', string.punctuation or string.digits))
-                    # print(text)
-                    # Tokenizing to unigrams
-                    token_text = word_tokenize(text)
-                    # print(new_text)
-                    # Remove stopwords using NLTK
-                    new_text_words = [
-                        word for word in token_text if word.lower() not in stopwords.words('english')]
-
-                    # Tokenize to bigrams
-                    bigrm_tokens = nltk.bigrams(token_text)
-                    # print(*map(' '.join, bigrm_tokens), sep=', ')
-
-    def preprocessing1(self, path):
         significant_token = []
         for part in tqdm(self.source_parts_dir_name):
             print("part : ", part)
